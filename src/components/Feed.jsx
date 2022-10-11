ab7add4e93b4107e9ec8b19fcfc33dab4b82bfb1
Sajal1005 <nandasajal.208@gmail.com>
2022-10-11 13:51:32 +0530

    videos fetching done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import { useState,useEffect } from 'react'
 import { Box, Stack, Typography } from '@mui/material'
-import Sidebar from './Sidebar';
-import Videos from './Videos';
+import { Videos, Sidebar } from "./";
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+      .then((data) => setVideos(data.items))
+    }, [selectedCategory]);
+
   return (
-    <div>
     <Stack className="overflow-hidden" sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-          <Sidebar/>
+      <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
 
           <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
             Copyright © 2022 MeTube
@@ -18,15 +28,14 @@ const Feed = () => {
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
         <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-          {/* {selectedCategory}  */}
-          <span style={{ color: "#FC1503" }}>videos</span>
+          {selectedCategory} 
+          <span style={{ color: "#FC1503" }}> videos</span>
         </Typography>
 
-        {/* <Videos videos={videos} /> */}
+        <Videos videos={videos} />
       </Box>
 
       </Stack>
-    </div>
   )
 }
 
