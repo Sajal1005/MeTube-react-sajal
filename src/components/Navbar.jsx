4973ba865d09d08a55756c3fcd35b91598e69a7f
Sajal1005 <nandasajal.208@gmail.com>
2022-10-11 19:10:45 +0530

    project completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => (
   <Stack className="z-30" direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link className="" to="/" style={{ display: "flex", alignItems: "center"}}>
       <img className="ml-5" src={logo} alt="logo" height={50} width={50} />
-      <h1 className="text-white text-3xl font-bold ml-5" >MeTube</h1>
+      <h1 className="text-white text-3xl font-bold ml-5 hidden sm:block" >MeTube</h1>
     </Link>
     <SearchBar />
   </Stack>
